da6a31c6133f1103e648f9e69d9c4d0ea5104f2b
shobhit17 <2013196@iiitdmj.ac.in>
2016-04-24 23:04:58 +0530

    Added files via upload

---
+++ formatROM.ino
@@ -0,0 +1,14 @@
+#include<EEPROM.h>
+void setup()
+{
+  Serial.begin(9600);
+  //int start=EEPROM.read(0);
+  //Serial.println(start,DEC);
+  EEPROM.write(0,0);
+  for(int i=1;i<=50;i++)
+  {
+    EEPROM.write(i,'-');
+    
+  }
+}
+void loop(){}
