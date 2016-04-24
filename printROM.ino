6f401fa119ce7080dfb50bc0397ba583fff96407
shobhit17 <2013196@iiitdmj.ac.in>
2016-04-24 23:06:34 +0530

    Added files via upload

---
+++ printROM.ino
@@ -0,0 +1,16 @@
+#include<EEPROM.h>
+void setup()
+{
+  Serial.begin(9600);
+  int start=EEPROM.read(0);
+  Serial.println(start,DEC);
+  for(int i=1;i<=start;i++)
+  {
+    Serial.print(i,DEC);
+    Serial.print("=");
+    Serial.print((char)EEPROM.read(i));
+    Serial.println();
+    
+  }
+}
+void loop(){}
